a5b8a0c9e1b53aa5c52653811965e8e7ed602afb
jesuslerma <shuyojl@gmail.com>
2018-04-30 19:46:14 -0500

    Add free lyrics project

---
+++ free-lyrics/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'Hola '
+    }
+  }
+}
+</script>
